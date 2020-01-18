91912cfbc4c596d7dcd712d25be9250d561a7894
miguelpengson <miguelpengson@gmail.com>
2020-01-18 17:37:52 +0800

    new file and route

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(350), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,30 @@
 from flask import render_template, url_for, request, redirect, flash
-from app import app
+from app import app, db
 from app.forms import NoteForm
+from app.models import Notes
 
 
 @app.route("/")
-@app.route("/index", methods=['GET', 'POST'])
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    notes = Notes.query.all()
+    return render_template('index.html', notes=notes)
+
 
 @app.route("/note/new", methods=['GET', 'POST'])    
 def new_note():
     form = NoteForm()
     if form.validate_on_submit():
+        note = Notes(title=form.title.data, content=form.content.data)
+        db.session.add(note)
+        db.session.commit()
         flash('Your note has been created!', 'success')
         return redirect(url_for('index'))
     return render_template('create_note.html', title='New Note', form=form, legend='New Note')
+
+
+@app.route("/note/<note_id>")
+def note(note_id):
+    note = Notes.query.get_or_404(note_id)
+    return render_template('note.html', title=note.title, note=note)
+    
