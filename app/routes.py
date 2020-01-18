c185cb5760705e8159dd52e2024ac3f384d5c3e7
miguelpengson <miguelpengson@gmail.com>
2020-01-18 21:54:59 +0800

    working, added delete and update routes, updating note.html

--- app/routes.py
+++ app/routes.py
@@ -28,3 +28,26 @@ def note(note_id):
     note = Notes.query.get_or_404(note_id)
     return render_template('note.html', title=note.title, note=note)
     
+@app.route("/note/<note_id>/update", methods=['GET', 'POST'])
+def update_note(note_id):
+    note = Notes.query.get_or_404(note_id)
+    form = NoteForm()
+    if form.validate_on_submit():
+        note.title = form.title.data
+        note.content =  form.content.data
+        db.session.commit()
+        flash('Your note has been updated!', 'success')
+        return redirect(url_for('index', note_id=note.id))
+    elif request.method == 'GET':
+        form.title.data = note.title
+        form.content.data = note.content
+    return render_template('create_note.html', title='Update Note', form=form,
+                            legend='Update Note')
+
+@app.route("/note/<note_id>/delete", methods=['POST'])
+def delete_note(note_id):
+    note = Notes.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    flash('Your note has been deleted!', 'success')
+    return redirect(url_for('index'))
